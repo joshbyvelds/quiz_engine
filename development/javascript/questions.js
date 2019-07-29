90a4e0f629a90dcd7afc863fe98fd29ebc4ba30a
Josh Byvelds <joshbyvelds@users.noreply.github.com>
2019-07-28 20:47:37 -0400

    Added New Questions

--- development/javascript/questions.js
+++ development/javascript/questions.js
@@ -1,12 +1,13 @@
-// Current Version 2;
+// Current Version 3;
 var question_categorys = [
+    "Unknown",
     "Rights and Responsibilities of Citizenship"
 ];
 
 var QUESTIONS = [
     {
         "ID": 0,
-        "Category": 0,
+        "Category": 1,
         "Question": "Name one source of Canadian law.",
         "Answers": [
             "United States common law",
@@ -19,7 +20,7 @@ var QUESTIONS = [
 
     {
         "ID": 1,
-        "Category": 0,
+        "Category": 1,
         "Question": "What is \"Magna Carta\"?",
         "Answers": [
             "The Great Charter of Freedoms",
@@ -32,7 +33,7 @@ var QUESTIONS = [
 
     {
         "ID": 2,
-        "Category": 0,
+        "Category": 1,
         "Question": "The Great Charter of Freedoms was signed in:",
         "Answers": [
             "France",
@@ -45,7 +46,7 @@ var QUESTIONS = [
 
     {
         "ID": 3,
-        "Category": 0,
+        "Category": 1,
         "Question": "When was the Magna Carta signed?",
         "Answers": [
             "1615",
@@ -58,7 +59,7 @@ var QUESTIONS = [
 
     {
         "ID": 4,
-        "Category": 0,
+        "Category": 1,
         "Question": "What does the Great Charter of Freedom include?",
         "Answers": [
             "Freedom of conscience and religion",
@@ -71,7 +72,7 @@ var QUESTIONS = [
 
     {
         "ID": 5,
-        "Category": 0,
+        "Category": 1,
         "Question": "What is \"Habeas corpus\"?",
         "Answers": [
             "The right to challenge unlawful detention by the state",
@@ -84,7 +85,7 @@ var QUESTIONS = [
 
     {
         "ID": 6,
-        "Category": 0,
+        "Category": 1,
         "Question": "When was the Constitution of Canada amended to include the Charter of Rights and Freedoms?",
         "Answers": [
             "1902",
@@ -97,7 +98,7 @@ var QUESTIONS = [
 
     {
         "ID": 7,
-        "Category": 0,
+        "Category": 1,
         "Question": "Who proclaimed the amended Constitution of Canada in 1982?",
         "Answers": [
             "The Prime Minister",
@@ -110,7 +111,7 @@ var QUESTIONS = [
 
     {
         "ID": 8,
-        "Category": 0,
+        "Category": 1,
         "Question": "What are the two principles upon which Canada is founded?",
         "Answers": [
             "The supremacy of God and freedom of speech",
@@ -123,7 +124,7 @@ var QUESTIONS = [
 
     {
         "ID": 9,
-        "Category": 0,
+        "Category": 1,
         "Question": "With which words does the Canadian Charter of Rights and Freedoms begin?",
         "Answers": [
             "\"Canada is a free country and home of the braves\"",
@@ -136,7 +137,7 @@ var QUESTIONS = [
 
     {
         "ID": 10,
-        "Category": 0,
+        "Category": 1,
         "Question": "What are \"mobility rights\"?",
         "Answers": [
             "Canadians have freedom of speech",
@@ -149,7 +150,7 @@ var QUESTIONS = [
 
     {
         "ID": 11,
-        "Category": 0,
+        "Category": 1,
         "Question": "Who can enter and leave the country freely without time constraints?",
         "Answers": [
             "Canadian citizens",
@@ -162,7 +163,7 @@ var QUESTIONS = [
 
     {
         "ID": 12,
-        "Category": 0,
+        "Category": 1,
         "Question": "Which three rights are included in the Canadian Charter of Rights and Freedoms?",
         "Answers": [
             "Mobility rights, Aboriginal Peoples' rights, and official language rights",
@@ -175,7 +176,7 @@ var QUESTIONS = [
 
     {
         "ID": 13,
-        "Category": 0,
+        "Category": 1,
         "Question": "French and English do not have equal status in Parliament and throughout the government",
         "Answers": [
             "True",
@@ -186,7 +187,7 @@ var QUESTIONS = [
 
     {
         "ID": 14,
-        "Category": 0,
+        "Category": 1,
         "Question": "Canadians work hard to respect:",
         "Answers": [
             "Marxism",
@@ -199,7 +200,7 @@ var QUESTIONS = [
 
     {
         "ID": 15,
-        "Category": 0,
+        "Category": 1,
         "Question": "What is a fundamental characteristic of the Canadian heritage and identity?",
         "Answers": [
             "Hockey",
@@ -212,7 +213,7 @@ var QUESTIONS = [
 
     {
         "ID": 16,
-        "Category": 0,
+        "Category": 1,
         "Question": "What are three responsibilities of Canadian citizenship?",
         "Answers": [
             "Being loyal to Canada, recycling all waste, and serving in the Canadian Forces.",
@@ -225,7 +226,7 @@ var QUESTIONS = [
 
     {
         "ID": 17,
-        "Category": 0,
+        "Category": 1,
         "Question": "When called to do so, serving on a jury is:",
         "Answers": [
             "A legal requirement",
@@ -236,7 +237,7 @@ var QUESTIONS = [
 
     {
         "ID": 18,
-        "Category": 0,
+        "Category": 1,
         "Question": "There is no compulsory military service in Canada",
         "Answers": [
             "True",
@@ -247,7 +248,7 @@ var QUESTIONS = [
 
     {
         "ID": 19,
-        "Category": 0,
+        "Category": 1,
         "Question": "What is a noble way to contribute to Canada and an excellent career choice?",
         "Answers": [
             "Learn both official languages",
@@ -256,6 +257,253 @@ var QUESTIONS = [
             "Serve in the regular Canadian Forces",
         ],
         "Correct": 3,
+    },
+
+    {
+        "ID": 20,
+        "Category": 0,
+        "Question": "In Canada's justice system what does \"presumption of innocence\" mean?",
+        "Answers": [
+            "Everyone is guilty until proven innocent",
+            "Guilt is decided by public opinion",
+            "Innocence is decided by public opinion",
+            "Everyone is innocent until proven guilty",
+        ],
+        "Correct": 3,
+    },
+
+    {
+        "ID": 21,
+        "Category": 0,
+        "Question": "What does the Canadian flag look like?",
+        "Answers": [
+            "Red and white with provincial emblems.",
+            "Red and white with a beaver.",
+            "White with a red border on each end and a red maple leaf in the centre.",
+            "Red with a white maple leaf.",
+        ],
+        "Correct": 2,
+    },
+
+    {
+        "ID": 22,
+        "Category": 0,
+        "Question": "Which animal is an official symbol of Canada?",
+        "Answers": [
+            "The moose",
+            "The hawk",
+            "The beaver",
+            "The deer",
+        ],
+        "Correct": 2,
+    },
+
+    {
+        "ID": 23,
+        "Category": 0,
+        "Question": "What is the capital city of Canada?",
+        "Answers": [
+            "Ottawa",
+            "Toronto",
+            "Montreal",
+            "Hull",
+        ],
+        "Correct": 0,
+    },
+
+    {
+        "ID": 24,
+        "Category": 0,
+        "Question": "One third of all Canadians live in which province?",
+        "Answers": [
+            "Quebec.",
+            "Ontario.",
+            "Northwest Territories.",
+            "Manitoba.",
+        ],
+        "Correct": 1,
+    },
+
+    {
+        "ID": 25,
+        "Category": 0,
+        "Question": "What are the three main types of industry in Canada?",
+        "Answers": [
+            "Natural resources, tourism and service industries.",
+            "Tourism, services and manufacturing.",
+            "Natural resources, tourism and manufacturing.",
+            "Natural resources, manufacturing and services.",
+        ],
+        "Correct": 3,
+    },
+
+    {
+        "ID": 26,
+        "Category": 0,
+        "Question": "Which party is the Official Opposition at the federal level?",
+        "Answers": [
+            "The New Democratic Party.",
+            "The Liberal Party.",
+            "The Independent Party.",
+            "The Conservative Party.",
+        ],
+        "Correct": 3,
+    },
+
+    {
+        "ID": 27,
+        "Category": 0,
+        "Question": "Give an example of where English and French have equal status in Canada.",
+        "Answers": [
+            "In schools.",
+            "In the workplace.",
+            "In the Parliament of Canada.",
+            "At City Hall.",
+        ],
+        "Correct": 2,
+    },
+
+    {
+        "ID": 28,
+        "Category": 0,
+        "Question": " What should you do if you do not receive a voter information card telling you when and where to vote?",
+        "Answers": [
+            "Go to the police station.",
+            "Call your Member of Parliament.",
+            "Assume you cannot vote.",
+            "Call Elections Canada or visit their website.",
+        ],
+        "Correct": 3,
+    },
+
+    {
+        "ID": 29,
+        "Category": 0,
+        "Question": "Which of the following statements about residential schools is NOT true?",
+        "Answers": [
+            "The federal government placed many Aboriginal children in residential schools to educate and assimilate them into mainstream Canadian culture.",
+            "The schools were poorly funded and inflicted hardship on the students.",
+            "The schools were welcomed by the Aboriginal people.",
+            "Aboriginal language and cultural practices were mostly prohibited.",
+        ],
+        "Correct": 2,
+    },
+
+    {
+        "ID": 30,
+        "Category": 0,
+        "Question": "Who are the Acadians?",
+        "Answers": [
+            "Aboriginal people of the arctic.",
+            "French-speaking Catholics living in Ontario.",
+            "The descendants of French colonists who began settling in what are now the Maritime provinces in 1604.",
+            "English speaking refugees who settled in Louisiana."
+        ],
+        "Correct": 2,
+    },
+
+    {
+        "ID": 31,
+        "Category": 0,
+        "Question": "Which of the following sentences best describes the War of 1812?",
+        "Answers": [
+            "Napoleon's fleet was defeated by the Royal Navy in the war.",
+            "The USA became independent from the British Empire after the war.",
+            "The USA invaded Canada and was defeated, which ensured that Canada would remain independent of the United States.",
+            "Canada joined the United States after the war.",
+        ],
+        "Correct": 2,
+    },
+
+    {
+        "ID": 32,
+        "Category": 0,
+        "Question": "Which of the following is NOT a feature of Canada's system of government?",
+        "Answers": [
+            "A federal state.",
+            "Parliamentary democracy.",
+            "Constitutional Monarchy.",
+            "Dictatorship."
+        ],
+        "Correct": 3,
+    },
+
+    {
+        "ID": 33,
+        "Category": 0,
+        "Question": "What is the significance of the discovery of insulin by Sir Frederick Banting and Charles Best? ",
+        "Answers": [
+            "Insulin is a hormone that permits you to eat anything you wish.",
+            "Insulin has saved 16 million lives worldwide.",
+            "Discovering insulin opened the doors to more discoveries.",
+            "Discovering insulin made Drs. Banting and Best famous."
+        ],
+        "Correct": 1,
+    },
+
+    {
+        "ID": 34,
+        "Category": 0,
+        "Question": "Which province is the main producer of pulp and paper and hydro-electricity?",
+        "Answers": [
+            "Quebec",
+            "Ontario",
+            "British Columbia",
+            "Manitoba",
+        ],
+        "Correct": 0,
+    },
+
+    {
+        "ID": 35,
+        "Category": 0,
+        "Question": "How is a Cabinet Minister chosen?",
+        "Answers": [
+            "By the Queen.",
+            "By the voters.",
+            "By other Cabinet Ministers.",
+            "By the Prime Minister",
+        ],
+        "Correct": 3,
+    },
+
+    {
+        "ID": 36,
+        "Category": 0,
+        "Question": "Canada has three territories and how many provinces?",
+        "Answers": [
+            "13",
+            "10",
+            "3",
+            "5",
+        ],
+        "Correct": 1,
+    },
+
+    {
+        "ID": 37,
+        "Category": 0,
+        "Question": "Name two responsibilities of the provincial and territorial government.",
+        "Answers": [
+            "Citizenship and Foreign Policy",
+            "Health and Education",
+            "Defense and Currency",
+            "Criminal Law and Interprovincial Trade,
+        ],
+        "Correct": 1,
+    },
+
+    {
+        "ID": 38,
+        "Category": 0,
+        "Question": "What was the \"Underground Railroad\"?",
+        "Answers": [
+            "An anti-slavery network that helped thousands of slaves escape the United States and settle in Canada",
+            "A railroad through the Rockies that was mainly through mountain tunnels",
+            "A network fur traders used to transport beaver pelts to the United States",
+            "The first underground subway tunnel in Toronto",
+        ],
+        "Correct": 0,
     },
 
     // {
